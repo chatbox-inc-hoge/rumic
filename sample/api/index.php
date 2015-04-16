10327743f2ce93256b8f68be210969e91a51595c
mikakane <mikakane2@gmail.com>
2015-04-16 15:47:24 +0900

    first commit

---
+++ sample/api/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkkn
+ * Date: 2015/04/16
+ * Time: 8:37
+ */
+
+require __DIR__."/../../vendor/autoload.php";
+
+$rumic = new \Chatbox\Rumic\Application();
+$rumic->loadEnv(__DIR__."/../../");
+$rumic->configure([
+    "bind" => [
+        ['Illuminate\Contracts\Debug\ExceptionHandler','App\Exceptions\Handler',true],
+        ['Illuminate\Contracts\Console\Kernel','App\Console\Kernel',true]
+    ],
+    "register"=>[
+        'App\Providers\AppRouteServiceProvider'
+    ]
+]);
+
+$rumic->run();
